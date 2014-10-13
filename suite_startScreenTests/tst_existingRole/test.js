e63097142760e6954d5db324edb664244c69339b
voodoo <voodoo@blackandyellowpages.com>
2014-10-13 09:40:50 -0600

    tightening up tests

--- suite_startScreenTests/tst_existingRole/test.js
+++ suite_startScreenTests/tst_existingRole/test.js
@@ -34,12 +34,21 @@ function main () {
 }
 
 function roleCardTests() {
+    /*
+    sendEvent("QMouseEvent", waitForObject(":Navigation.Role: *_QToolButton"), QEvent.MouseButtonPress, 109, 9, Qt.LeftButton, 1, 0);
+    activateItem(waitForObjectItem(":SocietyPro v*.Message Log_QMenu", "Professional"));
+    sendEvent("QMouseEvent", waitForObject(":Navigation.Role: *_QToolButton"), QEvent.MouseButtonPress, 140, 12, Qt.LeftButton, 1, 0);
+    activateItem(waitForObjectItem(":SocietyPro v*.Message Log_QMenu", "voodoo"));
+    */
+
     test.compare(object.exists(':qt_splithandle_.Role Card_HTML_Object'), true);
     test.compare(object.exists(':qt_splithandle_.Role Name_HTML_Object'), true);
     waitFor("object.exists(':qt_splithandle_.Role Name_HTML_Object')", 20000);
-    test.compare(findObject(":qt_splithandle_.Role Name_HTML_Object").visible, true);  
+    test.compare(findObject(":qt_splithandle_.Role Name_HTML_Object").visible, true); 
     test.compare(findObject(":qt_splithandle_.Role Name_HTML_Object").innerText, "VOODOO");
     test.compare(object.exists(':qt_splithandle_.Role Avatar_HTML_Object'), true);
     waitFor("object.exists(':qt_splithandle_.Role Avatar_HTML_Object')", 20000);
     test.compare(findObject(":qt_splithandle_.Role Avatar_HTML_Object").visible, true);   
+    
 }
+
